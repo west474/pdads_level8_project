02f2adcec594067d2b5074886d0b643e192b39eb
jmurphy172 <138017520+jmurphy172@users.noreply.github.com>
2024-11-03 13:45:09 +0000

    loaded raw data into main

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ Created on Sun Nov  3 13:21:18 2024
 @author: James
 """
 
+import pandas as pd
+
+from utils.loader import load_raw_data
+
+#%% load in data
+
+
+raw_chess_data = load_raw_data()
+
+
+
+
+#%% Clean data
+
+
+#%% Transform data
