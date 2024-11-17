c38125b31cc07501ac276635df2da129e462949f
jmurphy172 <138017520+jmurphy172@users.noreply.github.com>
2024-11-17 15:33:42 +0000

    Started on data transformation

--- manipulations/transformer.py
+++ manipulations/transformer.py
@@ -1,2 +1,22 @@
-# -*- coding: utf-8 -*-
+from utils.loader import load_raw_data
+
+from manipulations.cleaner import get_cleaned_chess_data
+
+
+
+#%% test module
+
+if __name__ == "__main__":
+    
+    RAW_DATA = load_raw_data()
+    
+    cleaned_data = get_cleaned_chess_data(RAW_DATA)
+    
+    
+    
+    
+
+
+
+
 
